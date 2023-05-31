370fb42d0de611217342a9aebaa783bcc365cc48
MarceloMFerreira <marcelomf.007@hotmail.com>
2023-05-31 10:04:54 -0300

    first commit

---
+++ src/app/pages/movies/movies.page.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { MovieService } from 'src/app/services/movie.service';
+
+@Component({
+  selector: 'app-movies',
+  templateUrl: './movies.page.html',
+  styleUrls: ['./movies.page.scss'],
+})
+export class MoviesPage implements OnInit {
+
+  constructor(private movieService: MovieService) { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/services/movie.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MovieService {
+
+  constructor(private http: HttpClient) { }
+
+  getTopRatedMovies(): Observable<any>{
+    return this.http.get('');
+
+  }
+
+  getMovieDetails(){}
+}
